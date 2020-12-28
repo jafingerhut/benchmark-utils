4f17235973ee1d14db08de8e537d5297e7634cf3
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-12-28 08:18:21 -0800

    Separate functionality of top from prepend-times.sh

--- scripts/top-with-times.sh
+++ scripts/top-with-times.sh
@@ -1,32 +1,34 @@
 #! /bin/bash
 
-# On this page:
-# https://stackoverflow.com/questions/21564/is-there-a-unix-utility-to-prepend-timestamps-to-stdin
+# Run top in batch mode (i.e. no screen redrawing) once every 2
+# seconds on all processes, until killed.
 
-# I learned about the `unbuffer` and `ts` commands, which are
-# available on Ubuntu Linux in packages `expect` and `moreutils`.
+######################################################################
+# On macOS 10.14.6 at least:
 
-# ts can format dates the same as the `strftime` C library call.  This
-# format looks straightforward to parse AND MERGE SORT for machines,
-# yet still readable for humans:
+# `-s 2` causes output to be generated once every 2 seconds.
 
-# %Y year in decimal including the century, e.g. 2019
-# %m month in decimal with leading 0, in range 01 to 12
-# %d the day of the month as a decimal number with leading 0, in range 01 to 31
+# Use `-l <num>` to limit it to print a report that many times.
 
-# %H the hour as a decimal number using a 24-hour clock, with leading
-#     0, range 00 to 23.
-# %M minute as a decimal number with leading 0, range 00 to 59
-# %S second as a decimal number with leading 0, range 00 to 60 to
-#     allow for occasional leap seconds
-# %.S microseconds as a decimal number with leading 0, range 000000 to
-#     999999
+# Use `-pid <pid>` to limit output to only the system-wide statistics,
+# plus information about that one process.  Without that, gives
+# information about all processes.
 
-# top -b -d 2 -p <pid> | TZ=UTC ts 'UTC %Y-%m-%d %H:%M:%S%.S'
+# top -l 0 -s 2
 
-# After `brew install moreutils` on macOS:
+######################################################################
+# On Ubuntu 18.04:
 
-# /usr/bin/top -l 5 -s 2 -pid 24043 | TZ=UTC ts 'UTC %Y-%m-%d %H:%M:%S%.S'
+# `-d 2` causes output to be generated once every 2 seconds.
+
+# Add `-n <num>` to limit it to print a report that many times before
+# exiting.
+
+# Use `-p <pid>` to limit output to only the system-wide statistics,
+# plus information about that one process.  Without that, gives
+# information about all processes.
+
+# top -b -d 2
 
 is_osx () 
 { 
@@ -44,28 +46,12 @@ function get_os() {
   done
 }
 
-which ts 2>&1 > /dev/null
-if [ $? != 0 ]
-then
-    2>&1 echo "No command 'ts' installed."
-    if [ `get_os` == "osx" ]
-    then
-	2>&1 echo "It can be installed using MacPorts or Homebrew"
-	2>&1 echo "in the package named 'moreutils'."
-    elif [ `get_os` == "ubuntu" ]
-    then
-	2>&1 echo "It can be installed using the command:"
-	2>&1 echo "    sudo apt-get install moreutils"
-    fi
-    exit 1
-fi
-
 if [ `get_os` == "osx" ]
 then
-    top -l 0 -s 2 | TZ=UTC ts 'UTC %Y-%m-%d %H:%M:%S%.S top: '
+    top -l 0 -s 2
 elif [ `get_os` == "ubuntu" ]
 then
-    top -b   -d 2 | TZ=UTC ts 'UTC %Y-%m-%d %H:%M:%S%.S top: '
+    top -b -d 2
 else
     2>&1 echo "Unknown OS not supported by this script."
     exit 1
